55022f2cda3194290a8ebc6da9589a9e25c4406a
Germanovich Pavel <germanovichpavel95@gmail.com>
2022-01-30 20:52:35 +0300

    feat: finish homework lesson 9

--- src/homework/lesson9/task3/StringUtil.java
+++ src/homework/lesson9/task3/StringUtil.java
@@ -2,6 +2,6 @@ package homework.lesson9.task3;
 
 public class StringUtil {
     public boolean isStringPolindrom(String str) {
-        return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
+        return str.length() > 1 && str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
     }
 }
