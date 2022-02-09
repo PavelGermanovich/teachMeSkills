b06e6291417d614a0167e40c29052db87b2e092b
Germanovich Pavel <germanovichpavel95@gmail.com>
2022-02-10 00:13:08 +0300

    feat: finish homework 12

--- src/homework/lesson12/Demo.java
+++ src/homework/lesson12/Demo.java
@@ -1,6 +1,11 @@
 package homework.lesson12;
 
 public class Demo {
+    /**
+     * В ваших проектах, сделать формирование отчета. В отдельном потоке,
+     * получаете все ваши объекты и формируете текстовый файл, в который выводите
+     * все ваши объекты
+     */
     public static void main(String[] args) {
         Thread vehiclesReportThread = new Thread(new TransportReportUtil());
         vehiclesReportThread.start();
